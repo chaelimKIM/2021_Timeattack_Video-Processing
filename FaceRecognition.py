c1fff80c028cca067c11013329352f0bde10c873
VitaYogu <83640171+VitaYogu@users.noreply.github.com>
2021-05-12 01:44:34 +0900

    Update FaceRecognition.py

--- FaceRecognition.py
+++ FaceRecognition.py
@@ -42,7 +42,7 @@ class FaceRecognition:
 
         return rects, shapes, shapes_np
 
-    def encode_faces(self, img, shapes):    # 68개의 점을 128개의 벡터 표현으로 변환
+    def encode_faces(self, img, shapes):    # 68개의 점을 128개의 벡터로 변환
         face_descriptors = []
         for shape in shapes:
             face_descriptor = facerec.compute_face_descriptor(img, shape)
@@ -55,13 +55,14 @@ class FaceRecognition:
 
         return img_rgb
 
-    def save_npy(self):    # npy 파일을 저장하는 함수
+    def save_npy(self, img_name):    # npy 파일을 저장하는 함수
+        # 메인과 다른 클래스에서 호출 시 다른 이름으로 descs를 저장하기 위해서 살짝 수정했습니다.
         for name, img_path in self.img_paths.items():
             img_rgb = self.bgr2rgb(cv2.imread(img_path))
 
             _, img_shapes, _ = self.find_faces(img_rgb)
             self.descs[name] = self.encode_faces(img_rgb, img_shapes)[0]
 
-        np.save('img/descs.npy', self.descs)
+        np.save('img/descs_' + img_name + '.npy', self.descs)
         #print(self.descs)
         print("npy save complete")
