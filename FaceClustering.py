e8743053f0cfefe8451aa82ab6447923561fcafa
VitaYogu <83640171+VitaYogu@users.noreply.github.com>
2021-05-29 18:30:15 +0900

    Update FaceClustering.py

--- FaceClustering.py
+++ FaceClustering.py
@@ -11,15 +11,19 @@ class FaceClustering:
     @staticmethod
     def clustering(img_path):
         files = os.listdir(img_path)
-        #print("files = ", files)
+        # print("files = ", files)
+        path = []
+        for i in files:
+            path.append(str(img_path) + str(i))
+        # print(path)
 
-        #얼굴 데이터
+        # 얼굴 데이터
         faces = []
-        #각각의 img에 몇명의 사람이 존재하는가
+        # 각각의 img에 몇명의 사람이 존재하는가
         faces_count = []
 
         for i in files:
-            img_rgb = Fr.bgr2rgb(cv2.imread(str(img_path) + str(i)))    # 인스턴스 선언 없이 Fr로 바로 사용가능
+            img_rgb = Fr.bgr2rgb(cv2.imread(str(img_path) + str(i)))
             _, shapes, _ = Fr.find_faces(img_rgb)
             n, descriptors = Fr.encode_faces(img_rgb, shapes)
             faces += descriptors
@@ -35,11 +39,32 @@ class FaceClustering:
 
         print("kmeans labels")
         print(kmeans.labels_)
-        # # label_ids = 얼굴 id
-        # label_ids = np.unique(clt.labels_)
-        # print("clt labels")
-        # print(clt.labels_)
-        # # num_unique_faces = label의 갯수
-        # num_unique_faces = len(np.where(label_ids > -1)[0])
-        #
-        # print("num_unique_faces = ", num_unique_faces)
+
+        rep_img_paths = []    # 대표이미지 경로 저장
+        image_label = []
+        cluster_count = 4    # 클러스터링 할 인물 수
+        c = 0
+        while c < cluster_count:
+            rep_img_paths.append('')
+            image_label.append(0)
+            c += 1
+
+        k = 0
+        c = 0
+        for i in faces_count:
+            for j in range(c, c+i):
+                # print(path[k])
+                id = kmeans.labels_[j]
+                if rep_img_paths[id] == '':
+                    rep_img_paths[id] = str(path[k])
+                    image_label[id] = j
+                else:
+                    if np.linalg.norm(kmeans.cluster_centers_[id] - faces[j]) < \
+                       np.linalg.norm(kmeans.cluster_centers_[id] - faces[image_label[id]]):
+                        rep_img_paths[id] = str(path[k])
+                        image_label[id] = j
+                c = c + i
+            k += 1
+
+        return rep_img_paths
+        # print(image_label)
