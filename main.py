4f19beb7d5f758ac0b77d4e1e13ba5d9ed9074a5
VitaYogu <83640171+VitaYogu@users.noreply.github.com>
2021-05-29 18:29:44 +0900

    Update main.py

--- main.py
+++ main.py
@@ -4,25 +4,21 @@ from VideoSlicing import VideoSlicing as Vs
 from FaceClustering import FaceClustering as Fc
 from DB import DB
 
-# main
-# DB = DB()
-# up_id = DB.select_upid_by_processing()
+DB = DB()
+up_id = DB.select_upid_by_processing()
 
-# up_vid_path = DB.select_upvidpath(up_id)
-# VS = Vs(up_vid_path, up_id)
-#
-# Fc.clustering("./slicing/" + str(up_id) + "/")
-#
-# FR = Fr()
-# up_img_path = DB.select_upimgpath(up_id)
-# up_img = cv2.imread(up_img_path)
-# clt_img_paths = []
-# for clt_img_path in clt_img_paths:
-#     clt_img = cv2.imread(clt_img_path)
-#     if FR.compare_faces(clt_img, up_img):
-#         result쪽으로 클러스터 이미지들 저장
-#         break
+up_vid_path = DB.select_upvidpath(up_id)
+VS = Vs(up_vid_path, up_id)
 
-# fC 클래스 테스트
-# img_path_dir = './slicing/'
-# Fc.clustering(img_path_dir)
+Fc.clustering("./slicing/" + str(up_id) + "/")
+
+FR = Fr()
+up_img_path = DB.select_upimgpath(up_id)
+up_img = cv2.imread(up_img_path)
+rep_img_paths = []
+rep_img_paths = Fc.clustering("./slicing/" + str(up_id) + "/")
+for clt_img_path in rep_img_paths:
+    clt_img = cv2.imread(clt_img_path)
+    if FR.compare_faces(clt_img, up_img):
+        # result에 저장
+        break
